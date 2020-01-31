5f049e1af9ebe138a023a427e24af5dd9f4ec4c3
Jakub Bartczuk <j.bartczuk@gidcoders.pl>
2020-01-31 12:30:33 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='subspace_clustering',
-    url='https://github.com/lambdaofgod/subspace-clustering',
+    url='https://github.com/lambdaofgod/subspace_clustering',
     author='Jakub Bartczuk',
     packages=find_packages(),
     install_requires=['spams']
